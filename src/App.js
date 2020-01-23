5a7e69ba833e876adfc488b727c17d8748efd1ef
Ben Koehler <bkoehler85282@gmail.com>
2020-01-22 22:01:52 -0700

    added basic function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+import Display from "./components/display.js";
+import Dashboard from "./components/dashboard.js";
 
 function App() {
+  const [homeHits, setHomeHits] = useState(0);
+  const [awayHits, setAwayHits] = useState(0);
+  const [balls, setBalls] = useState(0);
+  const [strikes, setStrikes] = useState(0);
+  const [outs, setOuts] = useState(0);
+  const [whosUp, setWhosUp] = useState("Home"); //determines if home(1) or away(2) is up;
+  const [innings, setInnings] = useState(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Display
+        homeHits={homeHits}
+        awayHits={awayHits}
+        balls={balls}
+        strikes={strikes}
+        outs={outs}
+        whosUp={whosUp}
+        innings={innings}
+      />
+      <Dashboard
+        setHomeHits={setHomeHits}
+        setAwayHits={setAwayHits}
+        setBalls={setBalls}
+        setStrikes={setStrikes}
+        setOuts={setOuts}
+        setWhosUp={setWhosUp}
+        setInnings={setInnings}
+        balls={balls}
+        strikes={strikes}
+        outs={outs}
+        whosUp={whosUp}
+        homeHits={homeHits}
+        awayHits={awayHits}
+      />
     </div>
   );
 }
